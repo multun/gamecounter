a730cf4259890734912dce180a7d102fa7b84013
Victor "multun" Collod <victor.collod@epita.fr>
2025-02-23 18:06:10 +0100

    make main menu buttons same width

--- app/src/main/java/net/multun/gamecounter/ui/main_menu/MainMenu.kt
+++ app/src/main/java/net/multun/gamecounter/ui/main_menu/MainMenu.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -28,7 +31,7 @@ fun MainMenuItem(name: String, baseColor: Color, onClick: () -> Unit) {
         color = baseColor.toDisplayColor(),
         modifier = Modifier.clickable(onClick = onClick)
     ) {
-        Text(name, modifier = Modifier.padding(20.dp))
+        Text(name, textAlign = TextAlign.Center, modifier = Modifier.padding(20.dp).fillMaxWidth())
     }
 }
 
@@ -44,7 +47,7 @@ fun MainMenu(viewModel: MainMenuViewModel, navController: NavController, modifie
 
             Column(
                 horizontalAlignment = Alignment.Start,
-                modifier = Modifier.wrapContentSize(),
+                modifier = Modifier.width(IntrinsicSize.Max),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 if (currentState.canContinue) {
