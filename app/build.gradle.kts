226be0c40187f54193745bfc0ad2e8a5ca3e6f43
Victor "multun" Collod <victor.collod@epita.fr>
2025-07-22 21:15:12 +0200

    release v2.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "net.multun.gamecounter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.5.1"
+        versionCode = 11
+        versionName = "2.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
